f1fd01e48b2d6bfa3b08c4a284fbe40b6d3a3612
Linus Lüssing <linus.luessing@c0d3.blue>
2025-12-14 16:00:33 +0100

    feat: create mcmap.bin from BGP IPv4/IPv6 multicast channels
    This would be helpful for us to immidiately monitor the BGP peerings for
    the IPv4/IPv6 multicast channels thanks to the "data" HTTP GET paramater,
    without needing to deploy an extra instance for now.

--- generator/cicd_gen.sh
+++ generator/cicd_gen.sh
@@ -3,7 +3,9 @@ set -ex
 registry_path="https://${GIT_DN42_TOKEN}@git.dn42.dev/dn42/registry.git"
 [ -e dn42registry ] && (cd dn42registry; git pull) || git clone "$registry_path" dn42registry --depth 1 --single-branch
 [ -e map.bin ] && rm map.bin
+config="$1"
+[ -z "$config" ] && config="config.sample.json"
 go mod download
 go build -o mapdn42
-cp config.sample.json config.json
+cp "$config" config.json
 ./mapdn42
